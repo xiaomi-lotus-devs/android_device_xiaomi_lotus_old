8d53bb67112d283b2f33b4381593a8cff6842da1
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-02 22:17:46 +0530

    lotus: Build libstdc++ from source
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I537b7362e53ca7f72bc5be40645235fb60417311

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    libstdc++.vendor
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
