27394f2072df79be5442e18fd4e3602bb88d7384
Hadenix <vsegda_online_007@bk.ru>
2023-10-08 14:56:32 +0300

    lotus: Initial skeleton
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

---
+++ lineage_lotus.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/lotus/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := lotus
+PRODUCT_NAME := lineage_lotus
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := MI PLAY
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
