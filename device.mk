09961a39bce6a037ca9e85836fc37d77e4a32d84
Hadenix <vsegda_online_007@bk.ru>
2024-08-18 11:37:33 +0300

    lotus: Build vendor variant of android.hardware.power@1.0
    * linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.mediatek.hardware.power@1.1-service": library "android.hardware.power@1.0.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -159,6 +159,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0.vendor
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.connectivity.rc \
