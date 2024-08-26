3bf70dd32fe5d06b7a2fd6a0cb1d66d434bf4688
Hadenix <vsegda_online_007@bk.ru>
2024-08-26 19:31:51 +0300

    lotus: Patchelf libsysenv.so to load VNDK 28 version of libbase.so
    * linker  : CANNOT LINK EXECUTABLE "/vendor/bin/sysenv_daemon": cannot locate symbol "_ZN7android4base10LogMessageC1EPKcjNS0_5LogIdENS0_11LogSeverityEi" referenced by "/vendor/lib/libsysenv.so"...

--- device.mk
+++ device.mk
@@ -207,6 +207,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-sp/libbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libbase-v28.so \
     prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-sp/libbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbase-v28.so
 
 PRODUCT_COPY_FILES += \
